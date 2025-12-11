094b154139e48fd567fd80cf903c2d1592f94e86
th3-st3w4rd <christians.green@gmail.com>
2025-12-11 15:21:55 -0500

    feat: add argparse to read available drives on windows

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import argparse
 import json
+import os
 
 from src.case_handler import CaseHandler
 
@@ -15,10 +16,14 @@ logging.basicConfig(
 def main():
     results = None
     parser= argparse.ArgumentParser()
+
+    os_name = CaseHandler.detect_detective_system()
+    drive_choices = CaseHandler.locate_drives(os_name.system.lower())
+    print(type(drive_choices))
     
     parser.add_argument("-i", "--internet", action="store_true", help="Searches the internet for extension stores.")
     parser.add_argument("-a", "--all-users", action="store_true", help="Searches all potential accounts on target.")
-    # parser.add_argument("-t", "--target-drive", action="store_true", help="Sets the target drive to search.")
+    parser.add_argument("-t", "--target-drive", choices=drive_choices, help="Sets the target drive to search.")
 
     parser.add_argument("-c", "--chrome", action="store_true", help="Searches locally for Google Chrome extensions.")
     # parser.add_argument("-e", "--edge", action="store_true", help="Searches locally for Microsoft Edge extensions.")
@@ -29,7 +34,7 @@ def main():
     """Initializes the `case_handler` """
     detective = CaseHandler(args)
 
-    print(json.dumps(detective.results, indent=4))
+    # print(json.dumps(detective.results, indent=4))
 
 
 if __name__ == "__main__":
--- src/case_handler.py
+++ src/case_handler.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import subprocess
 
 from src.supported_browsers import Chrome, Edge
 
@@ -11,8 +12,10 @@ class CaseHandler:
         self.results = dict()
         self.detective_machine_info = self.detect_detective_system()
         self.detective_os = self.detective_machine_info.system.lower()
-        self.perp_os = None
+        self.available_drive = self.locate_drives(self.detective_os)
+        self.target_drive = args.target_drive
         self.all_accounts = args.all_users
+        # print(self.target_drive)
 
         if self.args.chrome:
             chrome_browser = Chrome(
@@ -22,8 +25,18 @@ class CaseHandler:
             )
             self.results = chrome_browser.results
 
-    def detect_detective_system(self):
-        return platform.uname()
-
     def __repr__(self):
         return str(self.results)
+
+    @staticmethod
+    def detect_detective_system():
+        return platform.uname()
+
+    @staticmethod
+    def locate_drives(os_name: str):
+        os_command = {
+                "windows": ["fsutil", "fsinfo","drives"],
+        }
+        result = subprocess.run(os_command[os_name], capture_output=True, text=True)
+        available_drives = result.stdout.lower().split(" ")[1:-1]
+        return [drive.split(":")[0] for drive in available_drives]
